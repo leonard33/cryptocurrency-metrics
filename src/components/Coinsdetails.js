095491fdf6adc6f77c51479bb1ab63990682857f
leonard33 <leonardkombo14@gmail.com>
2022-06-23 15:51:41 +0300

    Adding api data to the user interface

--- src/components/Coinsdetails.js
+++ src/components/Coinsdetails.js
@@ -1,5 +1,4 @@
-/* eslint-disable no-unused-vars */
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 import millify from "millify";
 import { useGetCryptosDetailQuery } from "../redux/cryptoApi";
@@ -9,24 +8,14 @@ import { AiOutlineArrowRight } from "react-icons/ai";
 const Coinsdetails = () => {
   const { coinid } = useParams();
   const { data: coindetail, isFetching } = useGetCryptosDetailQuery(coinid);
-  // const [datastate, setDatastate] = useState(data?.data?.coins?);
-  console.log(coindetail?.data?.coins, "my detail");
-  console.log(coindetail?.data?.coins?.stats, "my stats");
 
   let coined = {};
-  let statistics = {};
+
   if (isFetching) return "Loading...";
 
   coined = coindetail?.data?.coins?.filter((item) => {
     return item.uuid == coinid;
   });
-
-  statistics = coindetail?.data?.coins?.stats?.filter((item) => {
-    return item.uuid == coinid;
-  });
-
-  console.log(coined);
-  console.log(statistics);
 
   return (
     <div className="details-container">
--- src/components/Cryptolist.js
+++ src/components/Cryptolist.js
@@ -1,60 +1,15 @@
-/* eslint-disable no-unused-vars */
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Cryptoitem from "./Cryptoitem";
-import { useDispatch } from "react-redux";
-import { cryptoApi, useGetCryptosQuery } from "../redux/cryptoApi";
+import { useGetCryptosQuery } from "../redux/cryptoApi";
 import { Link } from "react-router-dom";
 import "./Cryptolist.css";
 
 const Cryptolist = () => {
-  const dispatch = useDispatch();
   const { data: cryptodetaildata, isFetching } = useGetCryptosQuery();
-  const [cryptos, setCryptos] = useState(cryptodetaildata?.data?.coins);
-  /*
-  useEffect(() => {
-    // Add a subscription
-    const result = dispatch(useGetCryptosQuery().endpoints.getPost.initiate(postId))
 
-    // Return the `unsubscribe` callback to be called in the `useEffect` cleanup step
-    return result.unsubscribe
-  }, [dispatch, postId])
- 
-  useEffect(() => {
-    cryptodetaildata();
-  });
-  
-  useEffect(() => {
-    const options = {
-      method: "GET",
-      url: "https://coinranking1.p.rapidapi.com/coins",
-      params: {
-        referenceCurrencyUuid: "yhjMzLPhuIDl",
-        timePeriod: "24h",
-        "tiers[0]": "1",
-        orderBy: "marketCap",
-        orderDirection: "desc",
-        limit: "50",
-        offset: "0",
-      },
-      headers: {
-        "X-RapidAPI-Key": "0e23dfd609mshd4ac7e0b2521576p13a5adjsn09d2b8c5d60c",
-        "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
-      },
-    };
-    axios
-      .request(options)
-      .then(function (response) {
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  }, []);*/
+  const cryptos = cryptodetaildata?.data?.coins;
 
   if (isFetching) return "Loading...";
-
-  console.log(cryptos, "cryptos");
 
   return (
     <div className="cryptolist">
